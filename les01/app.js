2763560319c63ff4a89bbbe8fbf9a0f29ed16a1f
Joost <35542@ma-web.nl>
2022-11-22 12:25:44 +0100

    test

---
+++ les01/app.js
@@ -0,0 +1,23 @@
+class App
+{
+    runApplication()
+    {
+        let appNaam = "Hello"
+        let versienummer = (0,9)
+        let versiedatum = ("22-11-2022")
+        let autheur = ("Joost")
+        let copyright = ("string")
+        let distributeur = ("string")
+        let darkmode = (true)
+        console.log(appNaam)
+        console.log(versienummer)
+        console.log(versiedatum)
+        console.log(autheur)
+        console.log(copyright)
+        console.log(distributeur)
+        console.log(darkmode)
+
+    }
+}
+let app = new App();
+app.runApplication();
